289d9113152878ebfba44c4f9a506774a1e0c66e
Pierre <me@lebrunpierre.com>
2022-03-30 21:21:41 -0700

    jacked up branch

--- hangman_functions.py
+++ hangman_functions.py
@@ -1,34 +1,34 @@
-import utils.hangman_art as hangman_art
-
-
-# evaluates user input
-def user_evaluation(hangmanword, incorrect_guess=0):
-    user_input_bank = []
-    while incorrect_guess < len(hangman_art.stages):
-        userinput = user_input_validation()
-        if userinput in hangmanword:
-            print('Good guess')
-            user_input_bank.append(userinput)
-
-        else:
-            incorrect_guess += 1
-            print("Wrong")
-            print(hangman_art.stages[len(hangman_art.stages) - incorrect_guess])
-    print("You lost")
-
-
-def user_input_validation():
-    userinput = input("Type in a letter to start guessing! \n")
-    while userinput.isalpha() is False and len(userinput) == 1:
-        userinput = input("Please only guess one letter! \n").lower()
-    return userinput
-
-
-
-def display(hangmanword):
-    underscore = []
-    for letter in range(len(hangmanword)):
-        underscore.append('_')
-    print(underscore)
-
-
+# import utils.hangman_art as hangman_art
+#
+#
+# # evaluates user input
+# def user_evaluation(hangmanword, incorrect_guess=0):
+#     user_input_bank = []
+#     while incorrect_guess < len(hangman_art.stages):
+#         userinput = user_input_validation()
+#         if userinput in hangmanword:
+#             print('Good guess')
+#             user_input_bank.append(userinput)
+#
+#         else:
+#             incorrect_guess += 1
+#             print("Wrong")
+#             print(hangman_art.stages[len(hangman_art.stages) - incorrect_guess])
+#     print("You lost")
+#
+#
+# def user_input_validation():
+#     userinput = input("Type in a letter to start guessing! \n")
+#     while userinput.isalpha() is False and len(userinput) == 1:
+#         userinput = input("Please only guess one letter! \n").lower()
+#     return userinput
+#
+#
+#
+# def display(hangmanword):
+#     underscore = []
+#     for letter in range(len(hangmanword)):
+#         underscore.append('_')
+#     print(underscore)
+#
+#
--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import random
 import hangman_functions
 import utils.wordbank as wordbank
 import utils.hangman_art as hangman_art
+import classes.hangman_game as hangman
 
 import classes.player as player
 
+new_player = player.Player()
+new_hangman = hangman.HangManGame()
+# print to view the attributes
+# print(new_player.show())
+print(new_hangman.print_hangman())
+
+while True:
+    win_or_lose = new_hangman.fill_in_blanks(new_player)
+    if win_or_lose is not None:
+        break
